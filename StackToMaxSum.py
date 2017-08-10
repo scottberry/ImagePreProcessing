8c6d06e9da6a49d65d18b9d9d48e1cac7609f7e3
scottberry <scottdberry@gmail.com>
2017-08-10 11:44:59 +0200

    Added loggings and warnings ignore from skimage

--- StackToMaxSum.py
+++ StackToMaxSum.py
@@ -1,8 +1,18 @@
+import warnings
+import logging
 import skimage.io
 import numpy as np
-from os import path
+from os import path, getcwd
 from joblib import Parallel, delayed, cpu_count
 
+warnings.filterwarnings("ignore")
+
+logger = logging.getLogger('StackToMaxSum.py')
+hdlr = logging.FileHandler(path.join(getcwd(), 'StackToMaxSum.log'))
+formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
+hdlr.setFormatter(formatter)
+logger.addHandler(hdlr)
+logger.setLevel(logging.INFO)
 
 def project_single_site(base_dir, fname_stub, well_name, timeline_name,
                         field,
@@ -10,6 +20,8 @@ def project_single_site(base_dir, fname_stub, well_name, timeline_name,
                         output_channel_names, z_planes,
                         input_dir, output_dir):
 
+    logger.info('Processing channel %s, well %s, site %d',
+                input_channel_name, well_name, field)
     l_name = 'L01'
     field_name = 'F' + str(field).zfill(3)
 
@@ -28,6 +40,7 @@ def project_single_site(base_dir, fname_stub, well_name, timeline_name,
         path.join(base_dir, input_dir, in_name) for in_name in image_names
     ]
 
+    logger.debug('Well %s, site %d, image paths %s', well_name, field, image_paths)
     ic = skimage.io.imread_collection(image_paths)
     arr = skimage.io.concatenate_images(ic)
     sum_proj = np.sum(arr, axis=0, dtype=np.uint16)
@@ -45,7 +58,12 @@ def project_single_site(base_dir, fname_stub, well_name, timeline_name,
     max_path = path.join(base_dir, output_dir, max_name)
     sum_path = path.join(base_dir, output_dir, sum_name)
 
+    logger.info('Channel %s, well %s, Site %d, saving max projection: %s',
+                input_channel_name, well_name, field, max_path)
     skimage.io.imsave(fname=max_path, arr=max_proj)
+
+    logger.info('Channel %s, well %s, Site %d, saving sum projection: %s',
+                input_channel_name, well_name, field, sum_path)
     skimage.io.imsave(fname=sum_path, arr=sum_proj)
 
     return
