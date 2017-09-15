648150b6caf8127c1056a2936beb491d4e7a1851
scottberry <scottdberry@gmail.com>
2017-09-15 13:28:50 +0000

    Edits to RemovePersitentNoise.py to make it functional

--- RemovePersistentNoise.py
+++ RemovePersistentNoise.py
@@ -1,26 +1,28 @@
 import os
 import numpy as np
 import tifffile as tiff
-from scipy import misc
+from scipy.misc import toimage
 from random import sample
 
 STACK_dir = os.path.join(
-    os.path.expanduser('~'), 'shares', 'pelkmans-sc-storage',
+    os.path.expanduser('~'), 'pelkmans-sc-storage',
     'FXm', '20170823_Beads_FXm', 'STACKS'
 )
 
+n = 50
 n_sites = 160
 threshold = 150
 well_name = '3A'
 fname_stub = '20170823_Kim2_FXm_beads_3A1_w2sdcDAPImRFPxm-filter_s'
 
-selected_sites = sample(range(1, n_sites + 1), 1)
+selected_sites = sample(range(1, n_sites + 1), n)
 
-out_name = 'persistent_noise_' + well_name + '.png'
+out_name = 'persistent_noise_' + well_name + '_' + str(n) + '.png'
 out_path = os.path.join(STACK_dir, out_name)
 
 initial = True
 for site in selected_sites:
+    print 'site: ', site
     beads = tiff.imread(
         os.path.join(
             STACK_dir,
@@ -28,14 +30,14 @@ for site in selected_sites:
             fname_stub + str(site) + '.stk'
         )
     )
-    n_z = beads.shape[2]
-    beads_threshold = np.sum(beads3D > threshold, axis=2)
-    noise_mask = beads_threshold > (n_z / 3.0)
+    n_z = beads.shape[0]
+    beads_threshold = np.sum(beads > threshold, axis=0)
 
+    noise_mask = (beads_threshold > (n_z / 3.0)).astype(dtype=np.uint8)
     if initial:
         cumulative_mask = noise_mask
         initial = False
     else:
         cumulative_mask = cumulative_mask + noise_mask
 
-misc.imsave(name=out_path,arr=cumulative_mask)
+toimage(cumulative_mask, high=255, cmin=0, cmax=255).save(out_path)
