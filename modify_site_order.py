417508e46f2fe0135c81da57072106de3b9e11c9
scottberry <scottdberry@gmail.com>
2018-03-29 19:15:22 +0000

    Updated modify_site_order to number fields per well

--- modify_site_order.py
+++ modify_site_order.py
@@ -4,12 +4,10 @@ import time
 import warnings
 import logging
 import argparse
-import numpy as np
 import os
 import re
 import glob
 from MultiProcessingLog import MultiProcessingLog
-
 
 warnings.filterwarnings('ignore')
 logger = logging.getLogger()
@@ -52,30 +50,15 @@ def parse_arguments():
     return(parser.parse_args())
 
 
-def main(args):
-
-    # setup logging
-    formatter = logging.Formatter('%(asctime)s %(levelname)s | %(filename)s/%(funcName)s: %(message)s')
-    mp_log = MultiProcessingLog(
-        os.path.join(args.source_dir,
-                  'modify-site-order-' +
-                  time.strftime('%Y%m%d-%H%M%S') +
-                  '.log'),
-        'w', 0, 0
-    )
-    mp_log.setFormatter(formatter)
-    logger.addHandler(mp_log)
-
-    if not os.path.exists(args.target_dir):
-        os.makedirs(args.target_dir)
-
-    extant_filenames_C01 = sorted([os.path.basename(full_path) for full_path in glob.glob(args.source_dir + '*C01.tif')])
+def links_per_well(source_dir, filenames_C01):
 
     files_to_link = []
     new_site_number = 0
-    for fname_C01 in extant_filenames_C01:
+
+    for fname_C01 in filenames_C01:
         new_site_number += 1
-        for old_name in sorted(list_all_files_same_site(args.source_dir, fname_C01)):
+        old_names = sorted(list_all_files_same_site(source_dir, fname_C01))
+        for old_name in old_names:
             matches = re.match(pattern, old_name)
             new_name = (matches.group('stem') +
                         '_' + matches.group('well') +
@@ -87,11 +70,55 @@ def main(args):
                         matches.group('c') + '.tif')
             files_to_link.append((old_name,new_name))
 
+    return(files_to_link)
+
+
+def main(args):
+
+    if not os.path.exists(args.target_dir):
+        os.makedirs(args.target_dir)
+
+    # setup logging
+    formatter = logging.Formatter('%(asctime)s %(levelname)s | %(filename)s/%(funcName)s: %(message)s')
+    mp_log = MultiProcessingLog(
+        os.path.join(args.target_dir,
+                  'modify-site-order-' +
+                  time.strftime('%Y%m%d-%H%M%S') +
+                  '.log'),
+        'w', 0, 0
+    )
+    mp_log.setFormatter(formatter)
+    logger.addHandler(mp_log)
+
+    # get all names for channel 01
+    all_filenames_C01 = sorted([os.path.basename(full_path) for full_path in glob.glob(args.source_dir + '*C01.tif')])
+
+    # get unique wells
+    well_names = [m.group('well') for f in all_filenames_C01 for m in [re.match(pattern,f)] if m]
+    unique_well_names = sorted(list(set(well_names)))
+
+    # generate a list of tuples containing files to be linked
+    files_to_link = []
+    for well in unique_well_names:
+
+        # split list by well_name
+        well_regex = re.compile('.*_' + well + '_')
+        fnames_C01 = filter(well_regex.match, all_filenames_C01)
+
+        # generate list of links per well
+        files_to_link.extend(
+            links_per_well(args.source_dir, fnames_C01)
+        )
+
     for fname_pair in files_to_link:
-        os.link(os.path(args.source_dir,fname_pair[0]),
-                os.path(args.target_dir,fname_pair[1]))
+        logger.info('creating %s as link to %s',
+                    os.path.join(args.target_dir,fname_pair[1]),
+                    os.path.join(args.source_dir,fname_pair[0]))
+        os.link(os.path.join(args.source_dir,fname_pair[0]),
+                os.path.join(args.target_dir,fname_pair[1]))
 
     return
+
 
 if __name__ == "__main__":
     args = parse_arguments()
